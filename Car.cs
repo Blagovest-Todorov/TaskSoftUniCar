ca396ff6ba258e2d7ddd2c2c387a019f6e29d693
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-06-10 19:36:42 +0300

    add Program.cs, StartUp

---
+++ Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+           // this.TravelledDistance = 0;
+
+        }
+
+        public string Model { get; private set; }
+        public double FuelAmount { get; private set; }
+        public double FuelConsumptionPerKm { get; private set; }
+        public double TravelledDistance { get; set; }
+
+        public void Drive(double dictanceInKms) 
+        {
+            double consumedFuel = (FuelConsumptionPerKm * dictanceInKms);
+            bool canDrive = FuelAmount - consumedFuel >= 0;
+
+            if (canDrive == true)
+            {
+                FuelAmount -= consumedFuel;
+                TravelledDistance += dictanceInKms;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {            
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carData[0];
+                double fuelAmount = double.Parse(carData[1]);
+                double fuelConsumForOneKm = double.Parse(carData[2]);
+
+                //Car car = new Car(model, fuelAmount, fuelConsumForOneKm);
+                 cars.Add(new Car(model, fuelAmount, fuelConsumForOneKm));
+            }
+
+            while (true)
+            {
+                string comand = Console.ReadLine();
+
+                if (comand == "End")
+                {
+                    //ToDo
+                    break;
+                }
+
+                string[] comandData = comand
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string currComand = comandData[0];
+                string currCarModel = comandData[1];
+                double distanceInkms = double.Parse(comandData[2]);
+
+                if (currComand == "Drive")
+                {
+                     cars.Where(car => car.Model == currCarModel)
+                        .ToList()
+                        .ForEach(car => car.Drive(distanceInkms));
+                }                
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
